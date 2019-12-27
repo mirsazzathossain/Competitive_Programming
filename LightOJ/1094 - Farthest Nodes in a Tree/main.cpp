a35c3c70dcb24718f383535aa143ce4c2f340904
mirsazzathossain <mirsazzathossain@gmail.com>
2019-12-27 20:45:31 +0600

    Setup!!

---
+++ LightOJ/1094 - Farthest Nodes in a Tree/main.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Inputs
+#define sfi(x) scanf("%d", &x)
+#define sfl(x) scanf("%ld", &x)
+#define sfd(x) scanf("%lf", &x)
+#define sfc(x) scanf("%c", &x)
+#define sfs(x) getline(cin, x);
+#define in(x) cin>>x;
+//Outputs
+#define pfi(x) printf("%d", x)
+#define pfl(x) printf("%ld", x)
+#define pfd(x) printf("%lf", x)
+#define pfc(x) printf("%c", x)
+#define out(x) cout<<x
+#define endl printf("\n")
+#define pcase(i) printf("Case %d: ", i)
+//Loops
+#define loop0(i, n) for(i=0; i<n; i++)
+#define loop1(i, n) for(i=1; i<=n; i++)
+#define loopab(i, a, b) for(i=a; i<=b; i++)
+#define loopr0(i, n) for(i=n-1; i>=0; i--)
+#define loopr1(i, n) for(i=n; i>=1; i--)
+#define looprab(i, a, b) for(i=b; i>=a; i--)
+#define iterate(it, x) for(it=x.begin(); it!=x.end(); it++)
+//Fast Input Output
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+//Data Types
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+typedef vector<double> vd;
+typedef set<int> si;
+typedef set<long long> sll;
+typedef set<double> sd;
+typedef set<string> ss;
+typedef pair<int, int> pii;
+typedef map<int, int> mpii;
+typedef map<string, int> mpsi;
+//Keywords
+#define pb push_back
+#define ins insert
+//Useful Functions
+#define sort(x) sort(x.begin(), x.end());
+#define sortr(x, a, b) sort(x.begin()+a, x.end()+b+1);
+#define sortd(x) sort(x.begin(), x.end(), greater<int>());
+#define rev(x) reverse(x.begin(), x.end());
+#define revr(x, a, b) reverse(x.begin()+a, x.end()+b+1);
+//Constants
+const int MOD = 1000000007;
+const int MAX = 1000005;
+const double PI = acos(-1.0);
+
+
+
+int main(){
+    //fastio;
+    fstream fin("input.txt");
+    ofstream fout("output.txt");
+
+
+    
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Competitive_Programming
 
+## cpp.json
+You can use this as a snippets for setup your code faster in your editor. As i use VS Code the steps for set it as user snippets is very easy. 
+1. Open -> VS Code
+2. Click -> File
+3. Click -> Preference
+4. Click -> User Snippets,
+5. Search for -> cpp.json
+6. From Dropdown Select -> cpp.json
+7. Hit Enter
+8. Clear everything from your -> cpp.json
+9. Copy this file and past to -> cpp.json
+10. Save the file and Close it
+All done now whenever you want to start a new code just create a cpp file and type cpp and hit enter. You will get lines of code to start with. Happy Coding :) !!
+
+## BFS
+1. [UVA 10653 - Bombs! NO they are Mines!!](https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=18&page=show_problem&problem=1594)
+
 ## DFS
-1. [LightOj 1094 - Farthest Nodes in a Tree](http://lightoj.com/volume_showproblem.php?problem=1094?target=_blank)
+1. [LightOj 1094 - Farthest Nodes in a Tree](http://lightoj.com/volume_showproblem.php?problem=1094)
